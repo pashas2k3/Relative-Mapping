edadf335a3f35f58ac147a65614720757d51dca6
Vikram Gigoo <vgigoo@g.harvard.edu>
2016-10-27 21:02:25 -0400

    Information Removal

--- RelativeDatabaseTestCommands.sql
+++ RelativeDatabaseTestCommands.sql
@@ -1,19 +1,19 @@
 
 -- Relative Details insertion
 INSERT INTO RelativeDetails (first_name, middle_name, last_name, address, nickname, gender) VALUES
-('Rima', NULL, 'Kaul', 'Pune', NULL, 0);
-('Vikram', NULL, 'Gigoo', 'Boston', NULL, 1),
-('Angela', NULL, 'Kaul', 'Boston', NULL, 0),
-('Virendar', NULL, 'Gigoo', 'New Delhi', NULL, 1);
+('R', NULL, 'K', 'P', NULL, 0);
+('V', NULL, 'G', 'B', NULL, 1),
+('A', NULL, 'K', 'B', NULL, 0),
+('V', NULL, 'G', 'ND', NULL, 1);
 
 -- Relative Mapping information
 
 INSERT INTO ParentChildRelationship(parentId, childId) VALUES
-((SELECT id FROM RelativeDetails WHERE first_name = 'Virendar' AND last_name = 'Gigoo'),
-(SELECT id FROM RelativeDetails WHERE first_name = 'Vikram' AND last_name = 'Gigoo')),
-((SELECT id FROM RelativeDetails WHERE first_name = 'Rima' AND last_name = 'Kaul'),
-(SELECT id FROM RelativeDetails WHERE first_name = 'Angela' AND last_name = 'Kaul'))
+((SELECT id FROM RelativeDetails WHERE first_name = 'V' AND last_name = 'G'),
+(SELECT id FROM RelativeDetails WHERE first_name = 'V' AND last_name = 'G')),
+((SELECT id FROM RelativeDetails WHERE first_name = 'R' AND last_name = 'K'),
+(SELECT id FROM RelativeDetails WHERE first_name = 'A' AND last_name = 'K'))
 ;
 
 -- Find Relationships
-SELECT parent.first_name, child.first_name FROM RelativeDetails parent INNER JOIN ParentChildRelationship ON parent.id = parentId  INNER JOIN RelativeDetails child ON child.id = childId WHERE child.first_name = 'Vikram';
+SELECT parent.first_name, child.first_name FROM RelativeDetails parent INNER JOIN ParentChildRelationship ON parent.id = parentId  INNER JOIN RelativeDetails child ON child.id = childId WHERE child.first_name = 'V';
--- server_script.js
+++ server_script.js
@@ -1,12 +1,15 @@
 // The family tree and relationships can be represented as a DAG
 
 
-// Tasks
-// 1. Code for graph representation
-//     
-// 2. Code for path representation
 
-// 3. Code for read/write from database
+
+
+// Tasks
+// 1. Code for read/write from database
+//
+// 2. Code for graph representation
+//     
+// 3. Code for path representation
 
 "use strict";
 // Self invoking function at the top
@@ -19,54 +22,64 @@
         this.spouse= [];
     }
 
-    var sqlite3 = require('sqlite3').verbose();
-    var db = new sqlite3.Database('RelativeDatabase');
-
-
     
-    //create and remove in the graph
-    this.add = function()
-    {
-	//TODO: Make sure the id is passed in and it is valid
-	switch(arguments[0]) {
-	case "node":
-	    // Create a node here
-	    aNode = new Node();
-	    aNode.id = argument[1];
-	    break;
+    //Database manipulation
+    this. = function(relativeIdentifier){
+	'SELECT id FROM RelativeDetails WHERE first_name = \'' '\' OR last_name = \'Gigoo\''),
 
-	case "child":
-	    // Check the id exists
-	    child = argument[1]
-	    
-	    findInDatabase(argument[2])
-	    break;
+	
+    }
 
-	case "parent":
-	    
-	    break;
+    this.databaseManipulation = function(cmdStr){
 
-	case "sibling":
-	    
-	    break;
+    	if (cmdStr.split(" ",1) === "get"){
+    	    this.db.get(cmdStr);
+    	}
 
-	default:
-	    break;
-	}
+	
+    }
 
-	return 
-    };
-
-    this.remove = function(){
-	switch(arguments[0])
-	{
-	    
-	}
-    };
 
     
     try{
         // open the database
+	var sqlite3 = require('sqlite3').verbose();
+	this.db = new sqlite3.Database('RelativeDatabase');
+
+
+
+	db.serialize(
+	    function(){
+
+		function RelativeInfo() {		    
+		    RelativeInfo.genderOptions = ["male","female","unknown"];
+		    this.first_name = '';
+		    this.middle_name = '';
+		    this.last_name = ''
+		    this.address =''
+		    this.nickname =''
+		    this.gender = 0;
+		};
+
+		// 1. Inserting new record
+		var relative = new RelativeInfo();
+		relative.first_name = 
+
+
+		sqlStatement = 'INSERT INTO RelativeDetails (first_name, middle_name, last_name, address, nickname, gender) VALUES
+(\''+relative.first_name+'\', \'' +relative.middle_name+'\', 
+\''+relative.last_name+ '\', \''+relative.address+'\', \''+relative.nickname+ '\', '+relative.gender+ ');';
+
+		db.run(sqlStatement);
+
+		
+		//2. Getting new record
+
+	    }
+
+	);
+
+
         // Depending upon operation selected - findPath or manipulate Database
 	argument_list = arguments;
 	option = arguments_list.splice(0,1);

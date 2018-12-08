cf0b76f88e9ec9438e2038116ecd600e8a3795bf
Vikram <pashas.2k3@github.com>
2018-12-08 14:07:29 -0500

    Cleanup of the Python files

--- server/python_server/app.py
+++ server/python_server/app.py
@@ -16,23 +16,27 @@ def lambda_handler(event, context):
         API Gateway Lambda Proxy Output Format: dict
             'statusCode' and 'body' are required
         # api-gateway-simple-proxy-for-lambda-output-format
-        https: // docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+        https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     '''
     try:
         body_dict = json.loads(event.body)
 
         result = relevant_fn(event.resource, event.httpMethod) (body_dict)
+    except ValueError as e:
+        return {'statusCode': 400, 'body': "illegal value: {}".format(e)}
     except Exception as e:
-        print ("Caught exception" + e)
+        # May need something more interesting for 400, 500 in case we don't get correct translation
+        # out of the box
+        print('Caught exception {}'.format(e))
         raise e
 
     return {
         "statusCode": 200,
-        "body" : json.dumps({"result" : result})
+        "body": json.dumps({"result": result})
     }
 
 
-def relevant_fn(resource, httpMethod)
+def relevant_fn(resource, httpMethod):
     from .controller import add_individual, get_relative_info, make_relation, find_relation
     if resource.contains('relative'):
         if httpMethod == 'POST':
